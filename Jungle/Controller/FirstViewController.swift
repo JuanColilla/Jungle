786ddfcbd56e3c19b5f855e0ba8f9729f5d52c76
Juan Colilla <neodefensio@icloud.com>
2019-05-13 15:13:51 +0200

    Updated

--- Jungle/Controller/FirstViewController.swift
+++ Jungle/Controller/FirstViewController.swift
@@ -11,40 +11,146 @@ import AVFoundation
 
 class FirstViewController: UIViewController {
     
+    @IBOutlet weak var hyena: UIImageView!
+    @IBOutlet weak var lion: UIImageView!
+    @IBOutlet weak var bird: UIImageView!
+    @IBOutlet weak var hippo: UIImageView!
     @IBOutlet weak var volumeSlider: UISlider!
-    @IBAction func changeVolume(_ sender: UISlider) {
-        
-        audioPlayer?.setVolume(sender.value, fadeDuration: 0.1)
-    }
-    @IBAction func actionButtons(_ sender: UIButton) {
-        
-        let index = sender.tag
-        
-        switch index {
-        case 0:
-            audioPlayer?.play()
-        case 1:
-            audioPlayer?.stop()
-        case 2:
-            audioPlayer?.currentTime = 0
-            audioPlayer?.stop()
-        default:
-            break
-        }
-    }
-    var audioPlayer: AVAudioPlayer?
+    @IBOutlet weak var speakerImageView: UIImageView!
+    
 
+    var audioPlayer: AVAudioPlayer?
+    var onDemandAudioPlayer: AVAudioPlayer?
+    var onDemandPath: URL?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         guard let path = Bundle.main.path(forResource: "jungle_music", ofType: "mp3") else { return }
         
+        speakerImageView.image = UIImage.animatedImage(with: [UIImage(named: "Speaker 1"), UIImage(named: "Speaker 2"), UIImage(named: "Speaker 3"), UIImage(named: "Speaker 2"), UIImage(named: "Speaker 1")] as! [UIImage], duration: 1.5)
+        
         let url = URL(fileURLWithPath: path)
         audioPlayer = try? AVAudioPlayer(contentsOf: url, fileTypeHint: nil)
         audioPlayer?.prepareToPlay()
         audioPlayer?.setVolume(volumeSlider.value, fadeDuration: 0.1)
         audioPlayer?.play()
+        speakerImageView.startAnimating()
+        
+    }
+    
+    @IBAction func changeVolume(_ sender: UISlider) {
+        audioPlayer?.setVolume(sender.value, fadeDuration: 0.1)
+    }
+    
+    @IBAction func actionButtons(_ sender: UIButton) {
+        
+        let index = sender.tag
+        
+        switch index {
+        case 0:
+            speakerImageView.image = UIImage.animatedImage(with: [UIImage(named: "Speaker 1"), UIImage(named: "Speaker 2"), UIImage(named: "Speaker 3"), UIImage(named: "Speaker 2"), UIImage(named: "Speaker 1")] as! [UIImage], duration: 1.5)
+            audioPlayer?.play()
+            speakerImageView.startAnimating()
+            
+        case 1:
+            audioPlayer?.stop()
+            speakerImageView.stopAnimating()
+            speakerImageView.image = UIImage(named: "Speaker OFF")
+            
+        case 2:
+            audioPlayer?.currentTime = 0
+            speakerImageView.stopAnimating()
+            speakerImageView.image = UIImage(named: "Speaker OFF")
+            audioPlayer?.stop()
+            onDemandAudioPlayer?.stop()
+        default:
+            break
+        }
+    }
+    
+    @IBAction func tapGesture(_ sender: UITapGestureRecognizer) {
+        playSoundsOnDemand(animalID: sender.view?.tag ?? 0)
+    }
+    
+ 
+    @IBAction func swipeDown(_ sender: UISwipeGestureRecognizer) {
+            self.dropDown()
+    }
+    
+    @IBAction func tapTwiceToRun(_ sender: UITapGestureRecognizer) {
+            self.runawayFromHere()
+            onDemandAudioPlayer?.stop()
+    }
+    
+    
+    func playSoundsOnDemand(animalID: Int) {
+        onDemandAudioPlayer?.setVolume(volumeSlider.value, fadeDuration: 0.1)
+        switch animalID {
+        case 1:
+            onDemandPath = URL(string: Bundle.main.path(forResource: "hyena", ofType: "mp3")!)
+            onDemandAudioPlayer = try? AVAudioPlayer(contentsOf: onDemandPath!, fileTypeHint: nil)
+            onDemandAudioPlayer?.prepareToPlay()
+            onDemandAudioPlayer?.play()
+        case 2:
+            onDemandPath = URL(string: Bundle.main.path(forResource: "lion", ofType: "mp3")!)
+            onDemandAudioPlayer = try? AVAudioPlayer(contentsOf: onDemandPath!, fileTypeHint: nil)
+            onDemandAudioPlayer?.prepareToPlay()
+            onDemandAudioPlayer?.play()
+        case 3:
+            onDemandPath = URL(string: Bundle.main.path(forResource: "bird", ofType: "mp3")!)
+            onDemandAudioPlayer = try? AVAudioPlayer(contentsOf: onDemandPath!, fileTypeHint: nil)
+            onDemandAudioPlayer?.prepareToPlay()
+            onDemandAudioPlayer?.play()
+        case 4:
+            onDemandPath = URL(string: Bundle.main.path(forResource: "hippo", ofType: "mp3")!)
+            onDemandAudioPlayer = try? AVAudioPlayer(contentsOf: onDemandPath!, fileTypeHint: nil)
+            onDemandAudioPlayer?.prepareToPlay()
+            onDemandAudioPlayer?.play()
+        default:
+            print("Nothing...")
+        }
+    }
+    
+    // LINKED TO ANIMATION
+    func dropDown() {
+        let duration = 1.0
+        UIView.animate(withDuration: duration, animations: {
+            self.hyena.center.y += 500
+            self.lion.center.y += 500
+            self.bird.center.y += 200
+            self.hippo.center.y += 200
+        }, completion: { finnished in // HANDLE THIS NOT WORKING
+            self.hyena.center.y -= 500
+            self.lion.center.y -= 500
+            self.bird.center.y -= 200
+            self.hippo.center.y -= 200
+        })
+    }
+
+
+    func runawayFromHere() {
+        let duration = 0.75
+        UIView.animate(withDuration: duration, animations: {
+            self.hyena.center.y -= 400
+            self.hyena.center.x -= 400
+            self.lion.center.y -= 400
+            self.lion.center.x += 400
+            self.bird.center.y += 400
+            self.bird.center.x -= 400
+            self.hippo.center.y += 400
+            self.hippo.center.x += 400
+        }, completion: { finnished in // HANDLE THIS NOT WORKING
+            self.hyena.center.y += 400
+            self.hyena.center.x += 400
+            self.lion.center.y += 400
+            self.lion.center.x -= 400
+            self.bird.center.y -= 400
+            self.bird.center.x += 400
+            self.hippo.center.y -= 400
+            self.hippo.center.x -= 400
+        })
     }
 
 
