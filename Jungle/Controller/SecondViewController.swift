273293d14391e8701ab810202ef94c1a234fef60
Alumne <Alumne@iMac-de-user.local>
2019-05-13 16:48:21 +0200

    game finished

--- Jungle/Controller/SecondViewController.swift
+++ Jungle/Controller/SecondViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import AVFoundation
+import AVKit
 
 class SecondViewController: UIViewController {
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        //Load the files
+        let path = Bundle.main.path(forResource: "Jungle", ofType: "mp4")
+        
+        let videoURL = URL(fileURLWithPath: path!)
+        let player = AVPlayer(url: videoURL)
+        let playerViewController = AVPlayerViewController()
+        playerViewController.player = player
+        
+        self.present(playerViewController, animated: true){
+            playerViewController.player?.play()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
 }
 
